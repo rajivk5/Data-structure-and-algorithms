84f032b76bc9093a7c0cf854f99075cb8756c7ca
Rajiv <rajivr021@gmail.com>
2026-03-27 00:07:59 +0530

    updates

--- array.js
+++ array.js
@@ -15,7 +15,7 @@ var removeElement = function (nums, val) {
     return k;
 };
 
-// console.log(removeElement([3,2,2,3], 3));
+console.log(removeElement([3,2,2,3], 3));
 
 
 
@@ -293,4 +293,96 @@ var multiply = function (num1, num2) {
 };
 
 
-console.log(multiply('123', '456'));
+// console.log(multiply('123', '456'));
+
+
+
+//1st approach
+var findMedianSortedArrays = function (nums1, nums2) {
+     let arr = heapSort([...nums1, ...nums2])
+    let l = 0;
+    let r = arr.length - 1
+
+    if (arr.length % 2 == 0) {
+        let m1 = Math.floor((l + r) / 2);
+        let m2 = m1 + 1
+        let sum = arr[m1] + arr[m2]
+        return sum / 2
+    } else {
+        let m = (l + r) / 2;
+        return arr[m];
+    }
+};
+
+
+// console.log(findMedianSortedArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]));
+//9
+
+
+
+function heapSort(arr) {
+    let n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapifyDown(arr, i, n)
+    }
+
+    for (let i = n - 1; i > 0; i--) {
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+        heapifyDown(arr, 0, i)
+    }
+
+    return arr;
+}
+
+function heapifyDown(arr, i, n) {
+    let largest = i;
+    let left = (2 * i) + 1;
+    let right = (2 * i) + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapifyDown(arr, largest, n)
+    }
+
+}
+
+
+//2nd approach
+var findMedianSortedArrays = function(nums1, nums2) {
+    let n = nums1.length;
+    let m = nums2.length;
+
+    let total = n + m;
+    let mid1 = Math.floor((total - 1) / 2);
+    let mid2 = Math.floor(total / 2);
+
+    let i = 0, j = 0;
+    let count = 0;
+    let curr = 0, prev = 0;
+
+    while (count <= mid2) {
+        prev = curr;
+
+        if (i < n && (j >= m || nums1[i] <= nums2[j])) {
+            curr = nums1[i++];
+        } else {
+            curr = nums2[j++];
+        }
+
+        count++;
+    }
+
+    if (total % 2 === 0) {
+        return (prev + curr) / 2;
+    } else {
+        return curr;
+    }
+};
